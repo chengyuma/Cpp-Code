95feeb650f1a406cf5134bfa05ba116c301aebe7
Chengyu Ma <1802572599@qq.com>
2023-03-27 00:01:20 +0800

    add spinlock

--- demos/demo_spinlock.cpp
+++ demos/demo_spinlock.cpp
@@ -1,22 +1,22 @@
-#include "../spinlock.h" 
-#include <iostream>
-#include <vector>
-#include <thread>
+#include "../spinlock.h"
 #include <algorithm>
+#include <iostream>
+#include <thread>
+#include <vector>
 
 int main() {
-    SpinLock lock;
-    auto fun = [&lock](int i) {
-        for (int j = 0; j < 10; ++j) {
-            lock.Lock();
-            std::cout << j << ' ';
-            lock.Unlock();
-        }
-    };
-    std::vector<std::thread> v;
-    for (int i = 0; i < 5; ++i) {
-        v.emplace_back(fun, i);
+  SpinLock lock;
+  auto fun = [&lock](int i) {
+    for (int j = 0; j < 10; ++j) {
+      lock.Lock();
+      std::cout << j << ' ';
+      lock.Unlock();
     }
-    std::for_each(v.begin(), v.end(), [](auto &i) { i.join(); });
-    return 0;
+  };
+  std::vector<std::thread> v;
+  for (int i = 0; i < 5; ++i) {
+    v.emplace_back(fun, i);
+  }
+  std::for_each(v.begin(), v.end(), [](auto &i) { i.join(); });
+  return 0;
 }
