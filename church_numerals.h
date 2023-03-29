7f262b21095b7dfd37450327c5974837e4cab018
Chengyu Ma <1802572599@qq.com>
2023-03-29 16:04:34 +0800

    refine

--- church_numerals.h
+++ church_numerals.h
@@ -6,7 +6,10 @@ template <typename T> Apply<T> Zero() {
   return [](T x) { return x; };
 }
 
-template <typename T>
-Apply<T> AddOne(const Apply<T> &apply, const Apply<T> &num) {
-  return [&apply, &num](T x) { return apply(num(x)); };
+template <typename T> Apply<T> AddOne(Apply<T> apply, Apply<T> num) {
+  return [apply, num](T x) { return apply(num(x)); };
+}
+
+template <typename T> Apply<T> Add(Apply<T> num1, Apply<T> num2) {
+  return [num1, num2](T x) { return num1(num2(x)); };
 }
--- demos/demo_church_numerals.cpp
+++ demos/demo_church_numerals.cpp
@@ -8,9 +8,17 @@ int main() {
   auto zero = Zero<int>();
   auto one = AddOne<int>(apply, zero);
   auto two = AddOne<int>(apply, AddOne<int>(apply, zero));
+  auto ten = Zero<int>();
+  for (int i = 0; i < 10; i++) {
+    ten = AddOne<int>(apply, ten);
+  }
+  auto twelve = Add(two, ten);
 
+  std::cout << zero(0) << std::endl;
   std::cout << one(0) << std::endl;
   std::cout << two(0) << std::endl;
+  std::cout << ten(0) << std::endl;
+  std::cout << twelve(0) << std::endl;
 
   return 0;
 }
