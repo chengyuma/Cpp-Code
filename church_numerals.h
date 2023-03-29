9acd6486720f51b07f96596549d1b9a5ac864c31
Chengyu Ma <1802572599@qq.com>
2023-03-30 00:02:21 +0800

    refine

--- church_numerals.h
+++ church_numerals.h
@@ -6,11 +6,10 @@ template <typename T> Apply<T> Zero() {
   return [](T x) { return x; };
 }
 
-template <typename T>
-Apply<T> AddOne(const Apply<T> &apply, const Apply<T> &num) {
+template <typename T> Apply<T> AddOne(Apply<T> apply, Apply<T> num) {
   return [apply, num](T x) { return apply(num(x)); };
 }
 
-template <typename T> Apply<T> Add(const Apply<T> &num1, const Apply<T> &num2) {
+template <typename T> Apply<T> Add(Apply<T> num1, Apply<T> num2) {
   return [num1, num2](T x) { return num1(num2(x)); };
 }
