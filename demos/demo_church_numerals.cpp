3d2c31529e1db2ec798bb2560f747ded95969c20
Chengyu Ma <1802572599@qq.com>
2023-03-29 15:52:33 +0800

    update

--- demos/demo_church_numerals.cpp
+++ demos/demo_church_numerals.cpp
@@ -6,9 +6,11 @@ int main() {
   auto apply = [](int i) { return i + 1; };
 
   auto zero = Zero<int>();
-  auto one = AddOne<int>(apply, AddOne<int>(apply, zero));
+  auto one = AddOne<int>(apply, zero);
+  auto two = AddOne<int>(apply, AddOne<int>(apply, zero));
 
   std::cout << one(0) << std::endl;
+  std::cout << two(0) << std::endl;
 
   return 0;
 }
