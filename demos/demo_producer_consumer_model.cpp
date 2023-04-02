d2325ddad0e8b9242d945c955c7f61afee4dc677
Chengyu Ma <1802572599@qq.com>
2023-04-02 19:21:38 +0800

    refine

--- demos/demo_producer_consumer_model.cpp
+++ demos/demo_producer_consumer_model.cpp
@@ -3,26 +3,30 @@
 #include <iostream>
 #include <mutex>
 
+std::mutex io_mut;
+
 int main() {
   std::mutex mut;
-  ProducerConsumerModel<int> obj;
+  ProducerConsumerModel<int> obj(5);
   for (int i = 0; i < 10; i++) {
     obj.AddProducer([]() {
       int temp = random();
-      {
-        static std::mutex io_mut;
-        std::unique_lock<std::mutex> lock;
-        std::cout << "Produce: " << temp << std::endl;
-      }
       using namespace std::chrono_literals;
       std::this_thread::sleep_for(1000ms);
+      {
+        std::unique_lock<std::mutex> lock(io_mut);
+        std::cout << "Produce: " << temp << std::endl;
+      }
       return temp;
     });
   }
 
   for (int i = 0; i < 10; i++) {
     obj.AddConsumer([](auto i) {
-      std::cout << "Consume: " << i << std::endl;
+      {
+        std::unique_lock<std::mutex> lock(io_mut);
+        std::cout << "Consume: " << i << std::endl;
+      }
       using namespace std::chrono_literals;
       std::this_thread::sleep_for(500ms);
     });
--- producer_consumer_model.h
+++ producer_consumer_model.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <condition_variable>
+#include <cstddef>
 #include <functional>
 #include <mutex>
 #include <thread>
@@ -8,30 +9,40 @@
 
 template <typename T> class ProducerConsumerModel {
 public:
-  ProducerConsumerModel() : ready(false) {}
+  explicit ProducerConsumerModel(size_t buffer_size)
+      : buffer_size(buffer_size), used_buffer_size(0), begin(0), end(0) {
+    buffer.resize(buffer_size);
+  }
 
   void AddProducer(const std::function<T()> &fun);
   void AddConsumer(const std::function<void(T)> &fun);
   void JoinAllThread();
 
 private:
+  size_t updateIndex(size_t index) { return (index + 1) % buffer_size; }
+
   std::mutex mut;
   std::condition_variable consumer_cond;
   std::condition_variable producer_cond;
   std::vector<std::thread> producers;
   std::vector<std::thread> consumers;
-  T buffer;
-  bool ready;
+  std::vector<T> buffer;
+  size_t buffer_size;
+  size_t used_buffer_size;
+  size_t begin;
+  size_t end;
 };
 
 template <typename T>
 void ProducerConsumerModel<T>::AddProducer(const std::function<T()> &fun) {
   std::thread th([this, fun]() {
     std::unique_lock<std::mutex> lock(mut);
-    producer_cond.wait(lock, [this]() { return !ready; });
-    buffer = fun();
-    ready = true;
-    consumer_cond.notify_all();
+    producer_cond.wait(lock,
+                       [this]() { return used_buffer_size != buffer_size; });
+    buffer[end] = fun();
+    used_buffer_size++;
+    end = updateIndex(end);
+    consumer_cond.notify_one();
   });
 
   producers.push_back(std::move(th));
@@ -43,10 +54,11 @@ void ProducerConsumerModel<T>::AddConsumer(const std::function<void(T)> &fun) {
     T my_buffer;
     {
       std::unique_lock<std::mutex> lock(mut);
-      consumer_cond.wait(lock, [this]() { return ready; });
-      my_buffer = std::move(buffer);
-      ready = false;
-      producer_cond.notify_all();
+      consumer_cond.wait(lock, [this]() { return used_buffer_size > 0; });
+      my_buffer = std::move(buffer[begin]);
+      used_buffer_size--;
+      begin = updateIndex(begin);
+      producer_cond.notify_one();
     }
     fun(my_buffer);
   });
