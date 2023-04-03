508f12b7a82172c770686c21bfb155163990b74b
Chengyu Ma <1802572599@qq.com>
2023-04-03 21:58:21 +0800

    add LockFreeQueue

---
+++ lock_free_queue.h
@@ -0,0 +1,106 @@
+// #pragma once
+
+#include <atomic>
+#include <chrono>
+#include <memory>
+
+template <typename T> struct Node {
+  T data;
+  std::shared_ptr<Node> next;
+  std::atomic<bool> flag_;
+
+  template <typename U>
+  Node(U &&data) : data(std::forward<U>(data)), flag_(false) {}
+};
+
+template <typename T> class LockFreeStack {
+public:
+  LockFreeStack() = default;
+  LockFreeStack(const LockFreeStack &) = delete;
+  LockFreeStack(LockFreeStack &&) = delete;
+
+  template <typename U> void Push(U &&data);
+  bool Pop(T *data);
+
+private:
+  std::shared_ptr<Node<T>> head;
+};
+
+template <typename T>
+template <typename U>
+void LockFreeStack<T>::Push(U &&data) {
+  auto new_node = std::make_shared<Node<T>>(std::forward<U>(data));
+  new_node->next = std::atomic_load(&head);
+  while (
+      !std::atomic_compare_exchange_weak(&head, &(new_node->next), new_node)) {
+    ;
+  }
+}
+
+template <typename T> bool LockFreeStack<T>::Pop(T *data) {
+  std::shared_ptr<Node<T>> popped(std::atomic_load(&head));
+  while (popped &&
+         !std::atomic_compare_exchange_weak(&head, &popped, popped->next)) {
+    ;
+  }
+  if (popped == nullptr) {
+    return false;
+  }
+  *data = popped->data;
+  return true;
+}
+
+template <typename T> class LockFreeQueue {
+public:
+  template <typename... ArgsOfTsConstructor>
+  LockFreeQueue(ArgsOfTsConstructor &&...args) {
+    // make a dummy node
+    head = std::make_shared<Node<T>>(
+        T(std::forward<ArgsOfTsConstructor>(args)...));
+    tail = head;
+  };
+  LockFreeQueue(const LockFreeQueue &) = delete;
+  LockFreeQueue(LockFreeQueue &&) = delete;
+  LockFreeQueue &operator=(const LockFreeQueue &) = delete;
+  LockFreeQueue &operator=(LockFreeQueue &&) = delete;
+
+  template <typename U> void Push(U &&data);
+  bool Pop(T *data);
+
+private:
+  std::shared_ptr<Node<T>> head;
+  std::shared_ptr<Node<T>> tail;
+};
+
+template <typename T>
+template <typename U>
+void LockFreeQueue<T>::Push(U &&data) {
+  auto new_node = std::make_shared<Node<T>>(std::forward<U>(data));
+  std::shared_ptr<Node<T>> temp;
+  auto old_tail = std::atomic_load(&tail);
+  while (
+      old_tail->flag_.load(std::memory_order_acquire) ||
+      !std::atomic_compare_exchange_weak(&(old_tail->next), &temp, new_node)) {
+    temp.reset();
+    old_tail = std::atomic_load(&tail);
+  }
+  std::atomic_compare_exchange_weak(&tail, &old_tail, new_node);
+}
+
+template <typename T> bool LockFreeQueue<T>::Pop(T *data) {
+  std::shared_ptr<Node<T>> popped;
+  while (true) {
+    popped = std::atomic_load(&(head->next));
+    if (popped) {
+      popped->flag_.exchange(true);
+      if (std::atomic_compare_exchange_weak(&(head->next), &popped,
+                                            popped->next)) {
+        *data = popped->data;
+        std::atomic_compare_exchange_weak(&tail, &popped, head);
+        return true;
+      } else {
+        popped->flag_.exchange(false);
+      }
+    }
+  }
+}
