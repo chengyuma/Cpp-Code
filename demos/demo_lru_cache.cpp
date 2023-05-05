2bc57e8c6d62809eeaca8e70cae1ac2232f3fecb
Chengyu Ma <1802572599@qq.com>
2023-05-05 22:28:40 +0800

    add lru cache

---
+++ demos/demo_lru_cache.cpp
@@ -0,0 +1,36 @@
+#include "../lru_cache.h"
+#include <iostream>
+
+int main() {
+  LRUCache<int, int> cache(2);
+  int *val_ptr = new int;
+  cache.Put(1, 1);
+  cache.Put(2, 2);
+  if (cache.Get(1, val_ptr)) {
+    std::cout << "true: " << *val_ptr << std::endl;
+  } else {
+    std::cout << "false\n";
+  }
+  cache.Put(3, 3);
+  if (cache.Get(2, val_ptr)) {
+    std::cout << "true: " << *val_ptr << std::endl;
+  } else {
+    std::cout << "false\n";
+  }
+  cache.Put(4, 4);
+  if (cache.Get(1, val_ptr)) {
+    std::cout << "true: " << *val_ptr << std::endl;
+  } else {
+    std::cout << "false\n";
+  }
+  if (cache.Get(3, val_ptr)) {
+    std::cout << "true: " << *val_ptr << std::endl;
+  } else {
+    std::cout << "false\n";
+  }
+  if (cache.Get(4, val_ptr)) {
+    std::cout << "true: " << *val_ptr << std::endl;
+  } else {
+    std::cout << "false\n";
+  }
+}
---
+++ lru_cache.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <list>
+#include <unordered_map>
+
+template <typename Key, typename Value, typename Hash = std::hash<Key>,
+          typename KeyEqual = std::equal_to<Key>>
+class LRUCache {
+public:
+  explicit LRUCache(int cap) : cap_(cap) {}
+  LRUCache(const LRUCache &) = delete;
+  LRUCache(const LRUCache &&) = delete;
+  LRUCache &operator=(const LRUCache &) = delete;
+  LRUCache &operator=(const LRUCache &&) = delete;
+  bool Get(const Key &key, Value *val_ptr);
+  void Put(const Key &key, const Value &val);
+
+private:
+  void RefreshList(const Key &key);
+
+  using CacheList = std::list<std::pair<Key, Value>>;
+  int cap_;
+  CacheList list_;
+  std::unordered_map<Key, typename CacheList::iterator, Hash, KeyEqual> map_;
+};
+
+template <typename Key, typename Value, typename Hash, typename KeyEqual>
+void LRUCache<Key, Value, Hash, KeyEqual>::Put(const Key &key,
+                                               const Value &val) {
+  if (map_.count(key) == 0) {
+    list_.emplace_front(key, val);
+    map_[key] = list_.begin();
+    if (list_.size() > cap_) {
+      map_.erase(list_.back().first);
+      list_.pop_back();
+    }
+  } else {
+    RefreshList(key);
+    list_.front().second = val;
+  }
+}
+
+template <typename Key, typename Value, typename Hash, typename KeyEqual>
+bool LRUCache<Key, Value, Hash, KeyEqual>::Get(const Key &key, Value *val_ptr) {
+  if (map_.count(key) == 0) {
+    return false;
+  } else {
+    RefreshList(key);
+    *val_ptr = list_.front().second;
+    return true;
+  }
+}
+
+template <typename Key, typename Value, typename Hash, typename KeyEqual>
+void LRUCache<Key, Value, Hash, KeyEqual>::RefreshList(const Key &key) {
+  auto iter = map_[key];
+  if (iter != list_.begin()) {
+    list_.splice(list_.cbegin(), list_, iter, std::next(iter));
+  }
+}
